b4be0e6243b0f3e7096cf58040157e5977b7fbc2
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-07 15:10:26 +0000

    save

--- js/script.js
+++ js/script.js
@@ -3,3 +3,31 @@
 // Created by: George Khalil
 // Created on: May 2024
 // This file contains the JS functions for index.html
+
+
+function multiply() {
+  var num1 = parseInt(document.getElementById("num1").value)
+  var num2 = parseInt(document.getElementById("num2").value)
+  var result = 0
+
+  if (isNaN(num1) || isNaN(num2)) {
+    document.getElementById("result").innerHTML = "Please enter valid integers."
+    return
+  }
+
+  var absNum1 = Math.abs(num1)
+  var absNum2 = Math.abs(num2)
+
+  if (absNum1 === 0 || absNum2 === 0) {
+    result = 0
+  } else {
+    for (var i = 0; i < absNum2; i++) {
+      result += absNum1
+    }
+    if ((num1 < 0 && num2 > 0) || (num1 > 0 && num2 < 0)) {
+      result = -result
+    }
+  }
+
+  document.getElementById("result").innerHTML = "Result: " + result
+}
