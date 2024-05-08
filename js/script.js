fb711e47891b1f395b98634320f3c6c709dc285e
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-08 18:03:23 +0000

    save

--- js/script.js
+++ js/script.js
@@ -6,28 +6,30 @@
 
 
 function multiply() {
-  var num1 = parseInt(document.getElementById("num1").value)
-  var num2 = parseInt(document.getElementById("num2").value)
-  var result = 0
+  const num1 = parseInt(document.getElementById("num1").value)
+  const num2 = parseInt(document.getElementById("num2").value)
+  let result = 0
 
   if (isNaN(num1) || isNaN(num2)) {
     document.getElementById("result").innerHTML = "Please enter valid integers."
     return
   }
 
-  var absNum1 = Math.abs(num1)
-  var absNum2 = Math.abs(num2)
+  const absNum1 = Math.abs(num1)
+  const absNum2 = Math.abs(num2)
 
   if (absNum1 === 0 || absNum2 === 0) {
     result = 0
   } else {
-    for (var i = 0; i < absNum2; i++) {
+    let i = 0
+    while (i < absNum2) {
       result += absNum1
+      i++
     }
     if ((num1 < 0 && num2 > 0) || (num1 > 0 && num2 < 0)) {
       result = -result
     }
   }
 
-  document.getElementById("result").innerHTML = "Result:" + result 
+  document.getElementById("result").innerHTML = "Result:" + result
 }
